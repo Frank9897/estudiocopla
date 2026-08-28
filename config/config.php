e75163d76dc50a281e6964649b5f2f5158e956db
Vasquez Franco <116410138+Frank9897@users.noreply.github.com>
2026-08-28 19:21:07 -0300

    Centralizar datos de contacto

--- config/config.php
+++ config/config.php
@@ -3,58 +3,33 @@
 |--------------------------------------------------------------------------
 | Archivo: config.php
 |--------------------------------------------------------------------------
-| Configuración general del proyecto.
-| Centraliza las constantes que serán utilizadas en todo el sitio.
-|--------------------------------------------------------------------------
-*/
-
-/*
-|--------------------------------------------------------------------------
-| Información del sitio
+| Configuración general y datos compartidos del sitio.
 |--------------------------------------------------------------------------
 */
 
 define('SITE_NAME', 'Estudio COPLA');
-
 define('SITE_URL', 'http://localhost/estudiocopla');
-
 define('BASE_URL', '/estudiocopla');
 
-/*
-|--------------------------------------------------------------------------
-| Cache-busting de assets
-|--------------------------------------------------------------------------
-| asset_v() agrega ?v=<fecha de modificación> a css/js locales, para que
-| el navegador no sirva una versión vieja cacheada cuando el archivo
-| cambia durante el desarrollo.
-|--------------------------------------------------------------------------
-*/
-
+/* Cache-busting automático para css y js locales. */
 define('ROOT_PATH', dirname(__DIR__));
 
 function asset_v(string $relativePath): string
 {
     $absolutePath = ROOT_PATH . $relativePath;
-
     $version = is_file($absolutePath) ? filemtime($absolutePath) : time();
 
     return BASE_URL . $relativePath . '?v=' . $version;
 }
 
-/*
-|--------------------------------------------------------------------------
-| Información de contacto
-|--------------------------------------------------------------------------
-*/
+/* Datos de contacto centralizados para reutilizarlos en todo el sitio. */
+define('SITE_EMAIL', 'coplajujuy@gmail.com');
+define('SITE_PHONE', '+54 388 4480633');
+define('SITE_WHATSAPP', '543884480633');
+define('SITE_LOCATION', 'San Salvador de Jujuy, Argentina');
 
-define('SITE_EMAIL', 'contacto@estudiocopla.com.ar');
-
-define('SITE_PHONE', '+54 388 000-0000');
-
-/*
-|--------------------------------------------------------------------------
-| Zona horaria
-|--------------------------------------------------------------------------
-*/
+define('SITE_INSTAGRAM', 'https://www.instagram.com/estudio.copla');
+define('SITE_FACEBOOK', 'https://www.facebook.com/Estudio.copla');
+define('SITE_TIKTOK', 'https://www.tiktok.com/@estudio.copla');
 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
